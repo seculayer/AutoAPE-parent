03e8061f9b632cf57189b567e395f83703332618
yellowjin <yellowjin.dev@gmail.com>
2021-10-03 15:45:25 +0900

    오타 수정

--- install/02.initailize-master-node.sh
+++ install/02.initailize-master-node.sh
@@ -16,32 +16,6 @@ fi
 ##########################################################################
 source "${BASE_DIR}"/conf/ape.conf
 
-setupConfig() {
-        echo "--------------------------------"
-        echo "[ Set Namespace - ML]"
-        echo "--------------------------------"
-        while IFS= read -r line; do
-                OLD_APEML_NAMESPACE="[@namespace]"
-                escape_old=$(printf '%s\n' "${OLD_APEML_NAMESPACE}" | sed -e 's/[]\/$*.^[]/\\&/g')
-                escape_new=$(printf '%s\n' "${APEML_NAMESPACE}" | sed -e 's/[]\/$*.^[]/\\&/g')
-
-                sed -i "s/${escape_old}/${escape_new}/g" ${line} 2>/dev/null
-                if [ "$?" -eq "0" ] ; then echo "> ${line} is Applied!" ; fi
-        done < ".namespace_change_list"
-
-        echo "--------------------------------"
-        echo "[ Set Application path]"
-        echo "--------------------------------"
-        while IFS= read -r line; do
-                OLD_PATH="[@app_dir]"
-                escape_old=$(printf '%s\n' "${OLD_PATH}" | sed -e 's/[]\/$*.^[]/\\&/g')
-                escape_new=$(printf '%s\n' "${APP_DIR}" | sed -e 's/[]\/$*.^[]/\\&/g')
-
-                sed -i "s/${escape_old}/${escape_new}/g" ${line} 2>/dev/null
-                if [ "$?" -eq "0" ] ; then echo "> ${line} is Applied!" ; fi
-        done < ".apppath_change_list"
-}
-setupConfig
 
 ###########################################################################
 # init kubernetes master server
