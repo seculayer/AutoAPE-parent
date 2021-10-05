56fea53da06eb17e462052b627541e44c9a7ca93
jin.kim <jin.kim@seculayer.com>
2021-10-06 08:54:26 +0900

    DB Dockerfile

--- install/deploy/database/sql/APEAutoML.sql
+++ install/deploy/database/sql/APEAutoML.sql
@@ -1,3 +1,4 @@
+SET NAMES utf8mb4;
 CREATE TABLE VAR_FUNC_INFO (
        conv_func_id varchar(30) NOT NULL,
        conv_func_cls varchar(30) NULL,
@@ -8,9 +9,4 @@ CREATE TABLE VAR_FUNC_INFO (
        proc_id varchar(10) NULL,
        proc_dt varchar(14) NULL,
        user_made_yn varchar(1) NOT NULL DEFAULT 'N'
-);
-
-INSERT INTO VAR_FUNC_INFO VALUES(
-        '32', 'DevUsage', 'DevUsage', '29', '[[@dev_usage()]]',
-         '0', '장비의 사용률을 -1~1 사이로 Normalize 한다.', 'admin', '202108301306', 'N'
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ install/deploy/database/sql/APEAutoML_DATA.sql
@@ -0,0 +1,5 @@
+SET NAMES utf8mb4;
+INSERT INTO VAR_FUNC_INFO VALUES(
+    '32', 'DevUsage', '29', '[[@dev_usage()]]',
+    '0', '장비의 사용률을 -1~1 사이로 Normalize 한다.', 'admin', '202108301306', 'N'
+);
